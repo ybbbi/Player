c16f50d77b5fa9acb69c122f95df2efae5329227
张yanbo <zhangyanbo1996@icloud.com>
2020-01-12 19:48:41 +0800

    确定操作

--- qqdemo/src/main/java/com/ybbbi/qqdemo/view/fragment/DongTaiFragment.java
+++ qqdemo/src/main/java/com/ybbbi/qqdemo/view/fragment/DongTaiFragment.java
@@ -1,11 +1,16 @@
 package com.ybbbi.qqdemo.view.fragment;
 
+import android.app.AlertDialog;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.core.text.HtmlCompat;
 
 import com.hyphenate.chat.EMClient;
 import com.ybbbi.qqdemo.R;
@@ -42,17 +47,41 @@ public class DongTaiFragment extends BaseFragment implements LogoutIView, View.O
     @Override
     public void onLogout(boolean isSuccess, String s) {
         dissmissDialog();
-        if(isSuccess){
+        if (isSuccess) {
             startActivity(new Intent(getActivity(), LoginActivity.class));
             getActivity().finish();
-        }else{
-            ToastUtils.ShowMsg(s,getActivity());
+        } else {
+            ToastUtils.ShowMsg(s, getActivity());
         }
     }
 
     @Override
     public void onClick(View view) {
-        showDialog("退出登录中。。。");
-        logoutPresenter.Logout();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setTitle(R.string.logout);
+        if(Build.VERSION.SDK_INT>=24){
+        builder.setTitle(Html.fromHtml("<font  color='#c55553'>退出账户</font>", Html.FROM_HTML_MODE_LEGACY));
+
+        }else{
+
+        builder.setTitle(Html.fromHtml("<font color='#c55553'>退出账户</font>"));
+        }
+        builder.setMessage(R.string.logout);
+        //<font color=\"#C55553\">退出账户</font>
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                showDialog("退出登录中。。。");
+                logoutPresenter.Logout();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
     }
+
 }
